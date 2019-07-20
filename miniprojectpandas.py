f96f61b95a5abc36a8ecf709e86f883b8586d5f8
Zain Sharif <zain.m.sharif@gmail.com>
2019-07-20 22:12:29 +0200

    Read in GOOG, AAPL, AMZN csv and use date as row index, Adj Close as column

--- miniprojectpandas.py
+++ miniprojectpandas.py
@@ -6,3 +6,13 @@ goog_data = pd.read_csv("GOOG.csv")
 
 # We display the first 5 rows of the DataFrame
 print(goog_data.head(5))
+
+# We load the Google stock data into a DataFrame
+google_stock = pd.read_csv("GOOG.csv", index_col="Date", parse_dates=True, usecols=['Date','Adj Close'])
+print("\nGOOG.csv dataframe first 5 lines using Date as row index and Adj Close as column:\n",google_stock.head(5))
+# We load the Apple stock data into a DataFrame
+apple_stock = pd.read_csv("AAPL.csv", index_col="Date", parse_dates=True, usecols=['Date','Adj Close'])
+print("\nAAPL.csv dataframe first 5 lines using Date as row index and Adj Close as column:\n",apple_stock.head(5))
+# We load the Amazon stock data into a DataFrame
+amazon_stock = pd.read_csv("AMZN.csv", index_col="Date", parse_dates=True, usecols=['Date','Adj Close'])
+print("\nAMZN.csv dataframe first 5 lines using Date as row index and Adj Close as column:\n",amazon_stock.head(5))
